e8d1d63d099e0f8065feff1a7ea54e684f4aa4df
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-02-24 22:59:34 +0100

    test remise i calendar invite

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -6,24 +6,25 @@ class Meeting < ApplicationRecord
     scope :commande_courante, ->  (commande_courante) { where("commande_id = ?", commande_courante)}
     scope :client_courant, ->  (client_courant) { where("client_id = ?", client_courant)}
 
-    # def to_icalendar
+ #   include Rails.application.routes.url_helpers
 
-    #     # Create a calendar with an event (standard method)
-    #     cal = Icalendar::Calendar.new
-    #     cal.event do |e|
-    #     e.dtstart     = start_time
-    #     e.dtend       = end_time
-    #     e.summary     = name
-    #     e.description = "Have a long lunch meeting and decide nothing..."
-    #     e.ip_class    = "PRIVATE" #PUBLIC
-    #     e.location    = "adresse"
-    #     e.uid         = id.to_s
-    #     e.sequence    = Time.now.to_i
-    #     e.url         = meeting_url(self)
-    #     end
-        
-    #     cal.publish
-    #     cal.to_ical
-    # end
+ #   def to_icalendar
+
+        # Create a calendar with an event (standard method)
+ #       cal = Icalendar::Calendar.new
+ #       cal.event do |e|
+ #           e.dtstart     = start_time
+ #           e.dtend       = end_time
+ #           e.summary     = name
+ #           e.description = "Have a long lunch meeting and decide nothing..."
+ #""           e.ip_class    = "PRIVATE" #PUBLIC
+ #""           e.uid         = id.to_s
+ #           e.sequence    = Time.now.to_i
+ #           e.url         = meeting_url(self)
+ #       end
+ #       
+ #""        cal.publish
+ #""        cal.to_ical
+ #""    end
 
 end
--- app/services/meetings/icalendar_event.rb
+++ app/services/meetings/icalendar_event.rb
@@ -1,6 +1,7 @@
 #Meetings::IcalendarEvent.new(meeting: Meeting.first).call
+
 class Meetings::IcalendarEvent
-    require 'icalendar/tzinfo'
+    require 'icalendar'
 
     include Rails.application.routes.url_helpers
 
@@ -18,7 +19,7 @@ class Meetings::IcalendarEvent
     
 
             cal.event do |e|
-                e.dtstart     = @meeting.start_time.strftime("%Y%m%d""T""%H%M") #"20230124T151500Z"
+                e.dtstart     = @meeting.start_time
                 e.dtend       = @meeting.end_time 
                 e.summary     = @meeting.name
                 e.description = "Have a long lunch meeting and decide nothing..."
@@ -27,13 +28,13 @@ class Meetings::IcalendarEvent
                 e.uid         = "rdvid:#{@meeting.id.to_s}"
                 e.sequence    = Time.now.to_i
                 e.url         = meeting_url(@meeting)
-                e.organizer   = Icalendar::Values::CalAddress.new("mailto:#{ApplicationMailer.default_params[:from]}", cn: 'Yaro from Superails') 
+                e.organizer   = Icalendar::Values::CalAddress.new("mailto:#{ApplicationMailer.default_params[:from]}", cn: 'A1 soir app') 
                 e.attendee    = Icalendar::Values::CalAddress.new("mailto:kevin.hoffman.france@gmail.com", partstat: 'accepted') 
                 #e.status      = "CANCELLED"
             end
             
             cal.publish
-            cal.ip_method = "REQUEST"
+            #cal.ip_method = "REQUEST"
             cal.to_ical
         
         end 
