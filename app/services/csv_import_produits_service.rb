dfca945c1b1e6678839be57dd4b5be7efbe72411
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-04-20 12:25:14 +0200

    ajout image attachment to produit et prix vente en integer pour import

--- app/services/csv_import_produits_service.rb
+++ app/services/csv_import_produits_service.rb
@@ -1,29 +1,48 @@
 class CsvImportProduitsService
-    require 'csv'
-    def call(file)
-        file = File.open(file)
-        csv = CSV.parse(file, headers: true, col_sep: ',' )
-    
-        csv.each do |row|
-          produit_hash = {}
-          produit_hash[:nom] = row["Product Name"] 
-          produit_hash[:prix] = row["Variant Price"] 
-          produit_hash[:caution] = row["Variant Price"] # =prix vente à recupérer un integer ou decimal : enlever la ref euro
-          produit_hash[:description] = row["Product Description"] 
-          produit_hash[:categorie] = row["Product Categories"] #verif pas d'erreur de validation sur enum 
-          produit_hash[:taille] = row["Option1 Value"] #verif pas d'erreur validation
-          produit_hash[:quantite] = row["Variant Inventory"] # correspond au stock restant ? 
-         
-         # voir avec cha les correspondances possibles à ajouter : 
-          # produit_hash[:dateachat] = row[1] 
-          # produit_hash[:prixachat] = row[1] 
-          # produit_hash[:prixlocation] = row[1] 
-          # produit_hash[:couleur] = row[1] 
-          # produit_hash[:handle] = row[1] 
-          # produit_hash[:reffrs] = row[1] 
+  require 'csv'
+  require 'open-uri'
 
-          Produit.create(produit_hash)
+  def call(file)
+    file = File.open(file)
+    csv = CSV.parse(file, headers: true, col_sep: ',' )
+
+    csv.each do |row|
+      produit_hash = {}
+      produit_hash[:nom] = row["Product Name"]
+      produit_hash[:prix] = row["Variant Price"].delete('€').to_i
+      produit_hash[:caution] = row["Variant Price"].delete('€').to_i
+      produit_hash[:description] = row["Product Description"] 
+      produit_hash[:categorie] = row["Product Categories"]
+      produit_hash[:taille] = row["Option1 Value"]
+      produit_hash[:quantite] = row["Variant Inventory"]
+
+      # Attach main image to the record
+      main_image_url = URI.parse(row['Main Variant Image'])
+      main_image_filename = File.basename(main_image_url.path)
+      main_image = main_image_url.open
+      produit = Produit.new(produit_hash)
+      produit.image1.attach(io: main_image, filename: main_image_filename, content_type: ['image/png', 'image/jpeg'])
+
+      # Attach additional image to the record
+      if row['More Variant Images'].present?
+        additional_image_urls = row['More Variant Images'].split(',').map(&:strip)
+        additional_image_urls.each_with_index do |additional_image_url, index|
+          additional_image_filename = "additional_image_#{index}.jpg" # You can use any name for the additional images
+          additional_image = URI.parse(additional_image_url).open
+          produit.images.attach(io: additional_image, filename: additional_image_filename, content_type: ['image/png', 'image/jpeg'])
         end
-    end
+      end
 
+      produit.save
+    end
+  end
 end
+
+         
+  # voir avec cha les correspondances possibles à ajouter : 
+  # produit_hash[:dateachat] = row[1] 
+  # produit_hash[:prixachat] = row[1] 
+  # produit_hash[:prixlocation] = row[1] 
+  # produit_hash[:couleur] = row[1] 
+  # produit_hash[:handle] = row[1] 
+  # produit_hash[:reffrs] = row[1] 
